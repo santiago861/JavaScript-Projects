434250cda834b483823057f4b1ba59f5c3774c6f
santiago861 <santiago_reyes@ciencias.unam.mx>
2025-02-05 14:17:52 -0600

    change fixed(2) to prevent micro cents

--- cashRegister/script.js
+++ cashRegister/script.js
@@ -1,5 +1,4 @@
 // return change to the customer based on the price of the item, the amount of cash provided by the customer, and the amount of cash in the cash drawer
-
 let price = 1.87;
 let cid = [
   ['PENNY', 1.01],
@@ -12,26 +11,21 @@ let cid = [
   ['TWENTY', 60],
   ['ONE HUNDRED', 100]
 ];
-
-
 const currencyValues = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100];
-
+const changeText = document.getElementById('change-due');
+const input = document.getElementById('cash');
+const purchaseBtn = document.getElementById('purchase-btn');
+const textPrice = document.getElementById('price');
+textPrice.textContent = `Total: $${price}`;
+const cidContainer = document.getElementById('cid');
 let quantityCurrencies = [];
 for (let i = 0; i < currencyValues.length; i++) {
   quantityCurrencies.push(Math.round(cid[i][1] / currencyValues[i]));
 }
 
 
-const changeText = document.getElementById('change-due');
-const input = document.getElementById('cash');
-const purchaseBtn = document.getElementById('purchase-btn');
 
-const textPrice = document.getElementById('price');
-textPrice.textContent = `Total: $${price}`;
-
-const cidContainer = document.getElementById('cid');
-
-
+// Functions -------------------------------------------------------------------------
 const verifyInput = (inp) => {
   const entry = input.value;
   if (!entry) {
@@ -53,31 +47,51 @@ const updateScreenCashInDrawer = (arr) => {
 }
 updateScreenCashInDrawer(cid);
 
+
+
+
+
+
+
+
+
+
 const calculateChange = (customerMoney) => {
-  let changeDue = customerMoney - price + 0.001;
+  let changeDue = parseFloat((customerMoney - price).toFixed(2));
 
   for (let i = (cid.length) - 1; i >= 0; i--) {
-    let count = quantityCurrencies[i];
+    let coinsAvailable = quantityCurrencies[i];
     let coinsUsed = 0;
     
+    
     if (changeDue >= currencyValues[i]) {
-      while (count > 0 && changeDue >= currencyValues[i]) {
-        console.log(cid[i][0])
-        changeDue -= currencyValues[i];
-        count -= 1;
+      console.log(cid[i][0])
+      while (coinsAvailable > 0 && changeDue >= currencyValues[i]) {
+        console.log('run')
+        coinsAvailable -= 1;
         coinsUsed += 1;
-        quantityCurrencies[i] = count;
-        cid[i][1] = quantityCurrencies[i] * currencyValues[i]
+
+        quantityCurrencies[i] = coinsAvailable;
+        changeDue -= parseFloat(currencyValues[i].toFixed(2));
+        changeDue = parseFloat(changeDue.toFixed(2));
       }
+      cid[i][1] = parseFloat((quantityCurrencies[i] * currencyValues[i]).toFixed(2));
+      
       console.log(changeDue)
       changeText.innerHTML += `<p>${cid[i][0]}: $${currencyValues[i] * coinsUsed}</p>`
       console.log(quantityCurrencies)
-    } else {
-      console.log('some error')
     }
   }
   
 }
+
+
+
+
+
+
+
+
 
 const displayStatus = () => {
   changeText.innerHTML = ''
@@ -96,7 +110,7 @@ const displayStatus = () => {
 }
 
 
-// Event Listeners
+// Event Listeners -------------------------------------------------------------------------
 purchaseBtn.addEventListener('click', () => {
   updateScreenCashInDrawer(cid);
   if (verifyInput(input)) {
